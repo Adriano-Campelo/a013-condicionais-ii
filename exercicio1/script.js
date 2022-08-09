d2dc745c8489a3aee1cabbe56df5ca671021ec41
Adriano-Campelo <adriano.campelo@hotmail.com>
2022-08-09 16:31:04 -0300

    Exercicio de fixação

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,12 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número aqui.")) //seu numero aqui
 
+if(num%2===0){
+    console.log("Este número é divisível por 2")
+   
+} else if(num%3===0){
+    console.log("Este número  é divisível por 3")
+}
+
+if(num%2===0 || num%3===0){
+    console.log("Este número é divisível por 2 ou por 3")
+}
